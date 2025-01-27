e849bd559e883d06bb3aa98d9a786fd9aac9923b
rmsnandha <rmsnandha@example.com>
2025-01-27 07:24:17 -0600

    added junit vintage dependency

--- src/main/java/com/nk/algo/ArraysTest.java
+++ src/main/java/com/nk/algo/ArraysTest.java
@@ -5,7 +5,32 @@ import java.util.Arrays;
 public class ArraysTest {
 
     public static void main(String[] args) {
-        rotate(new int[]{1, 2, 3, 4}, 3);
+        //rotate(new int[]{1, 2, 3, 4}, 3);
+
+
+       int[] array =  { 3, 5, 7 };
+
+
+       /*
+          total rotate = 3
+            1st rotate = 7, 3, 5
+            2nd rotate = 5, 7, 3
+            3rd rotate = 3, 5, 7
+
+            arrLength = 2
+
+            rotateTime = 1
+            while rotateTime <=3
+                for/iterate array  from index 0 to length -1
+                   first =  arr[0]
+                   next =   arr[0 +1]
+
+
+
+
+        */
+
+
     }
 
 
